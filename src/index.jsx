7fa15458c4cc232184e3b22bc814a45153c4b831
Dmitry Andriushchenko <spaser15@outlook.com>
2019-12-03 18:07:17 +0200

    Added baseURL for production build

--- src/index.jsx
+++ src/index.jsx
@@ -8,5 +8,6 @@ import './index.css';
 
 const UNSPLASH_ACCESS_TOKEN = '2899a451fa9ea1ae1f8f39a41e0abea2df9518b35a79422013294bc0b4cbcfa6';
 axios.defaults.headers.Authorization = `Client-ID ${UNSPLASH_ACCESS_TOKEN}`;
+axios.defaults.baseURL = (process.env.NODE_ENV === 'production') ? 'https://api.unsplash.com' : '';
 
 ReactDOM.render(<App />, document.getElementById('root'));
